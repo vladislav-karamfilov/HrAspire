15422b26ba171391112f4fb7b0610a2ace4a27ed
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-05-26 17:08:51 +0300

    add blob + db Aspire resources.

--- HrAspire.AppHost/Program.cs
+++ HrAspire.AppHost/Program.cs
@@ -1,8 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.HrAspire_ApiService>("apiservice");
+var db = builder
+    .AddPostgres("db")
+    .WithDataVolume("db-data")
+    .AddDatabase("hr-aspire");
 
-builder.AddProject<Projects.HrAspire_Web>("webfrontend")
+var blobs = builder
+    .AddAzureStorage("storage")
+    .RunAsEmulator(c => c.WithDataVolume("blob-data"))
+    .AddBlobs("blobs");
+
+var apiService = builder
+    .AddProject<Projects.HrAspire_ApiService>("apiservice")
+    .WithReference(db)
+    .WithReference(blobs)
+    .WithReplicas(2);
+
+builder
+    .AddProject<Projects.HrAspire_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 

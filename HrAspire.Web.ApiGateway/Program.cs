c1bf62c02890500ff2e0ecb05e12eb473a64a165
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-07-07 07:47:38 +0300

    small enhancements

--- HrAspire.Web.ApiGateway/Program.cs
+++ HrAspire.Web.ApiGateway/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddNpgsql<EmployeesDbContext>("employees-db", opts => opts.MigrationsAssembly("HrAspire.Web.ApiGateway"));
+builder.Services.AddNpgsql<EmployeesDbContext>("employees-db", static opts => opts.MigrationsAssembly("HrAspire.Web.ApiGateway"));
 
 var app = builder.Build();
 

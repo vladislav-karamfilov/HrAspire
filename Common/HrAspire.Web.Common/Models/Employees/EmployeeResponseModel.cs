f5d7d2678b62d10ea04efff84ccfd0305fa8ee37
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-08-04 18:57:28 +0300

    No employee date of birth on all employees page.

--- Common/HrAspire.Web.Common/Models/Employees/EmployeeResponseModel.cs
+++ Common/HrAspire.Web.Common/Models/Employees/EmployeeResponseModel.cs
@@ -2,11 +2,4 @@
 
 using System;
 
-public record EmployeeResponseModel(
-    string Id,
-    string Email,
-    string FullName,
-    DateOnly DateOfBirth,
-    string? Department,
-    string Position,
-    DateTime CreatedOn);
+public record EmployeeResponseModel(string Id, string Email, string FullName, string? Department, string Position, DateTime CreatedOn);

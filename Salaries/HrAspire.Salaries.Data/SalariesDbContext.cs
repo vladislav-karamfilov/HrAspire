d40a9baf371e77d1ff5a50089b334ac81d190e23
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-08-23 12:43:58 +0300

    add salary request entity

---
+++ Salaries/HrAspire.Salaries.Data/SalariesDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace HrAspire.Salaries.Data;
+
+using HrAspire.Data.Common;
+using HrAspire.Salaries.Data.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class SalariesDbContext : DbContext
+{
+    public SalariesDbContext(DbContextOptions<SalariesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<SalaryRequest> SalaryRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<SalaryRequest>().Property(r => r.NewSalary).HasPrecision(precision: 18, scale: 6);
+
+        modelBuilder
+            .SetUtcKindToDateTimeProperties()
+            .SetGlobalIsDeletedQueryFilter()
+            .DisableForeignKeyCascadeDelete();
+    }
+}

c35b402f672a940aa63102d86d4f96245d032a7c
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-08-24 20:30:11 +0300

    add Garnet as cache

--- HrAspire.AppHost/Program.cs
+++ HrAspire.AppHost/Program.cs
@@ -1,10 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder
-    .AddPostgres("postgres")
-    .WithDataVolume("db-data")
-    .WithPgAdmin();
-
+var postgres = builder.AddPostgres("postgres").WithDataVolume("db-data").WithPgAdmin();
 var employeesDb = postgres.AddDatabase("employees-db", "employees");
 var salariesDb = postgres.AddDatabase("salaries-db", "salaries");
 var vacationsDb = postgres.AddDatabase("vacations-db", "vacations");
@@ -15,15 +11,28 @@ var azureStorage = builder
 
 var blobs = azureStorage.AddBlobs("blobs");
 
+var cache = builder
+    .AddGarnet("cache")
+    // TODO: .WithDataVolume("cache-data") // workaround for https://github.com/dotnet/aspire/issues/4870
+    .WithVolume("cache-data", "/data")
+    .WithArgs("--checkpointdir", "/data/checkpoints", "--recover", "--aof", "--aof-commit-freq", "60000");
+
 var employeesService = builder
     .AddProject<Projects.HrAspire_Employees_Web>("employees-service")
     .WithReference(employeesDb)
-    .WithReference(blobs);
+    .WithReference(blobs)
+    .WithReference(cache);
+
+var salariesService = builder
+    .AddProject<Projects.HrAspire_Salaries_Web>("salaries-service")
+    .WithReference(salariesDb)
+    .WithReference(cache);
 
 var apiGateway = builder
     .AddProject<Projects.HrAspire_Web_ApiGateway>("api-gateway")
     .WithReference(employeesDb)
     .WithReference(employeesService)
+    .WithReference(salariesService)
     .WithExternalHttpEndpoints();
 
 var apiGatewayEndpoint = apiGateway.GetEndpoint("https");

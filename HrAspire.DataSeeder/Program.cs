c9733254163600a2b3e2616a6240aa7df91a6ab0
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-08-30 14:22:41 +0300

    update Aspire packages.

--- HrAspire.DataSeeder/Program.cs
+++ HrAspire.DataSeeder/Program.cs
@@ -1,6 +1,9 @@
 using HrAspire.DataSeeder;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
 builder.Services.AddHostedService<DataSeederWorker>();
 
 var host = builder.Build();

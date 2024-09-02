951f24ea7a1ac3ad3f2f1a3e066e975ba0c44b77
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-09-02 11:23:31 +0300

    migrate DBs in DataSeeder project

--- HrAspire.DataSeeder/DataSeederWorker.cs
+++ HrAspire.DataSeeder/DataSeederWorker.cs
@@ -3,20 +3,72 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+using HrAspire.Business.Common;
+using HrAspire.Employees.Data;
+using HrAspire.Salaries.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 public class DataSeederWorker : BackgroundService
 {
+    private readonly IServiceScopeFactory scopeFactory;
+    private readonly IHostEnvironment hostEnvironment;
     private readonly IHostApplicationLifetime hostApplicationLifetime;
+    private readonly ILogger<DataSeederWorker> logger;
 
-    public DataSeederWorker(IHostApplicationLifetime hostApplicationLifetime)
+    public DataSeederWorker(
+        IServiceScopeFactory scopeFactory,
+        IHostEnvironment hostEnvironment,
+        IHostApplicationLifetime hostApplicationLifetime,
+        ILogger<DataSeederWorker> logger)
     {
+        this.scopeFactory = scopeFactory;
+        this.hostEnvironment = hostEnvironment;
         this.hostApplicationLifetime = hostApplicationLifetime;
+        this.logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        // TODO: migrate and seed data
-        await Task.CompletedTask;
+        if (this.hostEnvironment.IsDevelopment())
+        {
+            await this.MigrateAndSeedEmployeesDbAsync(stoppingToken);
+
+            await this.MigrateAndSeedSalariesDbAsync(stoppingToken);
+        }
 
         this.hostApplicationLifetime.StopApplication();
+    }
+
+    private async Task MigrateAndSeedEmployeesDbAsync(CancellationToken cancellationToken)
+    {
+        using var scope = this.scopeFactory.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<EmployeesDbContext>();
+
+        this.logger.LogInformation("Migrating employees DB...");
+        await dbContext.Database.MigrateAsync(cancellationToken);
+        this.logger.LogInformation("Migrated employees DB.");
+
+        // TODO: extract to separate method and seed more data - employees, documents(?)
+        if (!dbContext.Roles.Any())
+        {
+            dbContext.Roles.Add(new IdentityRole { Id = BusinessConstants.ManagerRole, Name = BusinessConstants.ManagerRole });
+            dbContext.Roles.Add(new IdentityRole { Id = BusinessConstants.HrManagerRole, Name = BusinessConstants.HrManagerRole });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+
+    private async Task MigrateAndSeedSalariesDbAsync(CancellationToken cancellationToken)
+    {
+        using var scope = this.scopeFactory.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<SalariesDbContext>();
+
+        this.logger.LogInformation("Migrating salaries DB...");
+        await dbContext.Database.MigrateAsync(cancellationToken);
+        this.logger.LogInformation("Migrated salaries DB.");
+
+        // TODO: seed data - salary requests?
     }
 }
--- HrAspire.DataSeeder/Program.cs
+++ HrAspire.DataSeeder/Program.cs
@@ -1,8 +1,20 @@
 using HrAspire.DataSeeder;
+using HrAspire.Employees.Data;
+using HrAspire.Salaries.Data;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
+
+builder.AddNpgsqlDbContext<EmployeesDbContext>(
+    "employees-db",
+    configureDbContextOptions: options => options.UseNpgsql(b => b.MigrationsAssembly(typeof(Program).Assembly.FullName)));
+
+builder.AddNpgsqlDbContext<SalariesDbContext>(
+    "salaries-db",
+    configureDbContextOptions: options => options.UseNpgsql(b => b.MigrationsAssembly(typeof(Program).Assembly.FullName)));
 
 builder.Services.AddHostedService<DataSeederWorker>();
 

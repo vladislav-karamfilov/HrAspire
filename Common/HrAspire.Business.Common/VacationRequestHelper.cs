e34f0b6b74f43eb77304794837329a4ebc83654b
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-09-27 08:56:00 +0300

    leave comments on some places

--- Common/HrAspire.Business.Common/VacationRequestHelper.cs
+++ Common/HrAspire.Business.Common/VacationRequestHelper.cs
@@ -13,6 +13,7 @@ public static class VacationRequestHelper
         var current = fromDate;
         while (current <= toDate)
         {
+            // In real world this would be more complicated - official holidays must also be handled
             if (current.DayOfWeek is not (DayOfWeek.Saturday or DayOfWeek.Sunday))
             {
                 workDays++;
--- Employees/HrAspire.Employees.Business/Employees/EmployeesService.cs
+++ Employees/HrAspire.Employees.Business/Employees/EmployeesService.cs
@@ -106,6 +106,7 @@ public class EmployeesService : IEmployeesService
         }
 
         var oldEmployeeFullName = employee.FullName;
+
         employee.FullName = fullName;
         employee.DateOfBirth = dateOfBirth;
         employee.Position = position;
@@ -149,7 +150,7 @@ public class EmployeesService : IEmployeesService
         }
         catch (Exception ex) when (ex is not RedisException)
         {
-            // Set the old employee full name just in case it's was updated before the exception
+            // Set the old employee full name just in case it was updated before the exception
             await this.CacheDatabase.HashSetAsync(BusinessConstants.EmployeeNamesCacheSetName, employee.Id, oldEmployeeFullName);
 
             throw;
